630ccb3e64e02e85e4a04690b10b7056c59addf2
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-06-02 23:19:09 -0500

    removes unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
-import 'package:steadycalendar/providers/session_provider.dart';
-import 'package:steadycalendar/repositories/cal_repo.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
 import 'app.dart';
 
 void main() async {
